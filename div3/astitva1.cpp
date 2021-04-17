6f1a52f9cf68ec5d2b87e1eb60dedc320770756b
Jatinrajpal <jatinrajpal57@gmail.com>
2021-04-18 01:18:07 +0530

    update

--- div3/astitva1.cpp
+++ div3/astitva1.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 int main()
 {
-	ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-    #endif
 	int n;
 	cin>>n;
 	if(n>1000000)
